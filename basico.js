dd61f23165f8bd8cfc02c6c166db8a985e2def62
BruksfildServices01 <132058136+BruksfildServices01@users.noreply.github.com>
2023-04-29 09:09:14 -0300

    codigos básicos
    base do Javascript

---
+++ basico.js
@@ -0,0 +1,30 @@
+var nome = "lucas";
+var n1 = 10;
+var n2 = 20;
+var n3 
+
+n3 = n1 + n2;
+
+if (n3 < 10) {
+    console.log(nome)
+  } else {
+    console.log("nao e o lucas");
+  }
+
+  n3 = 0;
+
+  for (n3 = 0; n3 < 5; n3++) {
+    console.log(n3);
+  }
+
+
+  while (n3 > 0) {
+    console.log(n3);
+    n3 = n3 - 1;
+  }
+
+  function soma(a, b) {
+    return a + b;
+  }
+
+  console.log(soma(3, 4));
